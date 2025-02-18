58a8d1603a0285db2a227677291566c08a3f123b
Lucky032 <laxmankarnam04@gmail.com>
2025-02-18 23:27:25 +0530

    added 3 files extra

---
+++ pattern_printing.py
@@ -0,0 +1,38 @@
+# for j in range(1, 6):
+#     for i in range(1, 6):
+#         print("*", end="") 
+#     print()
+
+
+# for i in range(1, 6):
+#     for j in range(1, i + 1):
+#         print("*", end="")
+#     print()
+
+
+for i in range(1, 6):
+    for j in range(1,i+1):
+       
+        print("*", end="")
+    print()
+
+
+for i in range(1,6):
+        print("*"*i)
+
+
+for i in range(1, 6):
+    for j in range(6, i, -1):
+        print("*", end="")
+    print()
+
+for i in range(5, 0, -1):
+    print("*" * i) 
+
+#number pattern
+for i in range(1,6):
+      for j in range(1,i+1):
+            print(j,end= "")
+      print()
+
+
